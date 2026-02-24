df8e12f0e60047e317b3147937a4d2054909345d
Mdhadiuzzaman0 <mdhadiuzzaman483@gmail.com>
2026-02-24 23:00:28 +0600

    3btn color change done

--- damn.js
+++ damn.js
@@ -16,19 +16,7 @@ const jobCounter=document.getElementById('updateJobCounter')
 
 
 // jobCounter 
-function updateJobCounter() {
-    const totalJobs = card.children.length;
 
-    if (currentstatus === 'allshowBtn') {
-        jobCounter.innerText = `${totalJobs} jobs`;
-    }
-    else if (currentstatus === 'interviewshowBtn') {
-        jobCounter.innerText = `${interviewList.length} of ${totalJobs} jobs`;
-    }
-    else if (currentstatus === 'rejectedshowBtn') {
-        jobCounter.innerText = `${rejectedList.length} of ${totalJobs} jobs`;
-    }
-}
 
 // 3btn color change
 function showOnly(id){
@@ -79,31 +67,9 @@ function showOnly(id){
 
 
 // not applied text change 
-const cards=document.querySelectorAll('#allcards > div')
-        for(const card of cards){
-            const interview =card.querySelector('.interviewBtn');
-            const rejected=card.querySelector('.rejectedBtn');
-            const status=card.querySelector('.status');
-
-            interview.addEventListener('click', function(){
-            status.innerText = "INTERVIEW";
-            status.classList.remove('bg-[#EEF4FF]', 'bg-[#EF4444]');
-            status.classList.add('bg-[#10B981]', 'text-white', 'rounded-2xl', 'text-center')});
-
-        rejected.addEventListener('click', function(){
-           status.innerText = "REJECTED";
-           status.classList.remove('bg-[#EEF4FF]', 'bg-[#10B981]');
-           status.classList.add('bg-[#EF4444]', 'text-white', 'rounded-2xl', 'text-center');
-        })
-    }
 
 
 
-const emptyTemplate = `
-<div class="text-center bg-white rounded-xl p-20">
-    <img src="./jobs.png" class="mx-auto mb-5">
-    <h1 class="text-[#002C5C] font-semibold text-[24px]">No jobs available</h1>
-    <p class="text-[#64748B]">Check back soon for new job opportunities</p>
-</div>
-`;
+
+
 
